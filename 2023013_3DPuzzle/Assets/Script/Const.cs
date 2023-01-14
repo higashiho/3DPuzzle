bd7c51f0df25dd4b5e6ee341a5b599570bfa2da6
higashihokakeru <kakeru.0422.pojr@gmail.com>
2023-01-15 03:46:18 +0900

    Camera挙動作成

--- 2023013_3DPuzzle/Assets/Script/Const.cs
+++ 2023013_3DPuzzle/Assets/Script/Const.cs
@@ -11,7 +11,7 @@ public class Const : MonoBehaviour
     // 以下Box用定数
     public const int CHECK_POS_X = 5;           // 接地判定用
     public const int CHECK_POS_Z = 5;           // 接地判定用
-    public const float BOX_MOVE_SPEED = 5f;     // プレイヤーに押された際に移動するまでの時間
+    public const float BOX_MOVE_SPEED = 3f;     // プレイヤーに押された際に移動するまでの時間
     public const float BOX_POS_Y = 4.5f;        // BoxのY座標
     // 以上Box用定数
 
