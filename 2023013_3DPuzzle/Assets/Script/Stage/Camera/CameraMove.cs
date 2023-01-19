6eb95c4a658739b4091b6344101a4d8c83284456
humihumiinoue <shyallandboill1212@gmail.com>
2023-01-19 09:38:58 +0900

    カメラ追従

--- 2023013_3DPuzzle/Assets/Script/Stage/Camera/CameraMove.cs
+++ 2023013_3DPuzzle/Assets/Script/Stage/Camera/CameraMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraMove
 {
@@ -10,10 +11,32 @@ public class CameraMove
     /// <param name="tmpCamera">カメラの実体</param> 
     public void Move(BaseCamera tmpCamera)
     {
-        var tmpPos = tmpCamera.transform.position;
-        var tmpPlayerPos = InGameSceneController.Player.transform.position;
+        var tmpPos = tmpCamera.transform.position;                            //カメラの座標
+        var tmpPlayerPos = InGameSceneController.Player.transform.position;   //プレイヤーの座標
+        
+        var tmpNewPos = new Vector3(tmpPlayerPos.x, tmpPos.y,tmpPlayerPos.z); //カメラの位置計算
+        tmpCamera.transform.position = tmpNewPos;                             //カメラの位置更新
 
-        var tmpNewPos = new Vector3(tmpPlayerPos.x, tmpPos.y,tmpPos.z);
-        tmpCamera.transform.position = tmpNewPos;
-    }
+        //要修正　かつ　Tweenがnullの時に入る(多重tween防止用)
+    //     if(tmpCamera.ActtinTween == null)
+    //     {
+    //         tmpCamera.ActtinTween = tmpCamera.transform.DOMove(
+    //             tmpPlayerPos,                                                   //プレイヤーの位置に
+    //             InGameSceneController.Player.PlayersData.PlayerMoveTime         //時間をかけて移動
+    //         ).SetEase(Ease.OutSine).OnComplete( () =>
+    //         {
+    //             reset(tmpCamera);
+    //         }
+    //         );
+    //     }
+    // }
+
+    // /// <summary>
+    // /// 初期化関数
+    // /// </summary>
+    // /// <param name="tmpCamera">カメラの実体</param>
+    // private void reset(BaseCamera tmpCamera)
+    // {
+    //     tmpCamera.ActtinTween = null;
+       }
 }
