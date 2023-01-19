de298a407cc72dfa76d4cf1c9d153c7ebd45597c
higashihokakeru <kakeru.0422.pojr@gmail.com>
2023-01-19 23:00:26 +0900

    ニードルタイル修正
    出現している場合にプレイヤーに当たったら初期座標に戻るように修正

--- 2023013_3DPuzzle/Assets/Script/Player/ColPlayer.cs
+++ 2023013_3DPuzzle/Assets/Script/Player/ColPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Scene;
+using DG.Tweening;
 
 namespace Player
 {
@@ -22,7 +23,9 @@ namespace Player
             // ニードルが表示されているニードルタイルを踏んだ時初期座標に移動
             if(col.gameObject.tag == "Needle" && col.transform.GetChild(0).gameObject.activeSelf)
             {
-                this.transform.position = InGameSceneController.Player.StartPos;
+                // ４秒後にスタート地点に戻る
+                this.transform.DORotate(Vector3.zero, Const.START_BACK_TIME).SetEase(Ease.Linear);
+                this.transform.DOMove(InGameSceneController.Player.StartPos, Const.START_BACK_TIME).SetEase(Ease.Linear);
             }
         }
     }
