2c594536edd93c7ff5df4b23b5996fd5a69c511b
humihumiinoue <shyallandboill1212@gmail.com>
2023-02-01 17:35:59 +0900

    カメラ修正

--- 2023013_3DPuzzle/Assets/Script/Scene/TitleSceneMove.cs
+++ 2023013_3DPuzzle/Assets/Script/Scene/TitleSceneMove.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 namespace Scene
 {
@@ -13,6 +12,31 @@ namespace Scene
             //タイトルシーンからメインシーンへ
             tmpScene.MoveFade.FadeIn(tmpScene, "MainScene");
             //tmpScene.StateScene = BaseScene.SceneState.Main;
-        } 
+        }
+
+        /// <summary>
+        /// マウスがボタンの上に乗ったら
+        /// 押せるようにする
+        /// </summary>
+        /// <param name="tmpTitle">BaseTitle</param>
+        public void OnMouseOver(BaseScene tmpTitle)
+        {
+            if(!tmpTitle.OnToButtonFlag)
+            {
+                tmpTitle.OnToButtonFlag = true;
+            }
+        }
+        /// <summary>
+        /// マウスがボタンの上から離れたら
+        /// 押せなくする
+        /// </summary>
+        /// <param name="tmpTitle">BaseTitle</param>
+        public void OnMouseExit(BaseScene tmpTitle)
+        {
+            if(tmpTitle.OnToButtonFlag)
+            {
+                tmpTitle.OnToButtonFlag = false;
+            }
+        }
     }
 }
